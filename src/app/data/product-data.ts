0adcc978878419c2009e9c2adeaf510029daa200
Will Berner <Will.o.berner@gmail.com>
2026-07-21 14:49:30 -0400

    Fixes to detail text

--- src/app/data/product-data.ts
+++ src/app/data/product-data.ts
@@ -13,7 +13,7 @@ export const PRODUCTS: Product[] = [
     id: 'bubble-boy',
     title: 'Bubble Boy',
     price: 60.0,
-    description: 'A sailor-inspired plushie with embroidered accents and a soft, collectible finish.',
+    description: 'A sweet sailor plushie who\'s searching the seas for his memories!',
     details: 'This funny little guy is 16cm tall and handmade with soft minky. His hat is even removable (but don\'t take it away from him, he likes it!) Buyers can choose from a plush with an embroidered face, a regular plush with a printed face, or a discounted plush with a slight color variation. Please allow 1-2 weeks for shipping; domestic shipping time is usually under one week while International shipping times can vary. NOTE: This product was made in a household with dogs. Also, as these are handmade, they may not be identical to the one pictured.',
     hero: 'assets/images/bubble-boy/Bubble-Boy-1.jpg',
     images: [
@@ -45,7 +45,7 @@ export const PRODUCTS: Product[] = [
     id: 'rose-girl',
     title: 'Rose Girl',
     price: 60.0,
-    description: 'A racing dinosaur jockey who\'s ready to win!',
+    description: 'A rosy plush with botanical details and bright pink hair for a bold collector display.',
     details: 'No matter what, this rosy girl will find her way! 16cm tall and handmade with soft minky, buyers can choose from a plush with an embroidered face or a printed face. Please allow 1-2 weeks for shipping; domestic shipping time is usually under one week while International shipping times can vary. NOTE: This product was made in a household with dogs. Also, as these are handmade, they may not be identical to the one pictured. ',
     hero: 'assets/images/rose-girl/Rose-Girl-1.jpg',
     images: [
@@ -76,7 +76,7 @@ export const PRODUCTS: Product[] = [
     id: 'wind-warrior',
     title: 'Wind Warrior',
     price: 60.0,
-    description: 'A solemn plushie awakened from his long slumber!',
+    description: 'A daring fighter who won\'t be defeated! ',
     details: 'Don\'t let his size fool you—this fearsome warrior has never been defeated! If you\'re not careful, he might just put a wedding ring (of death) around your heart <3. Handmade of soft minky and felt, this little guy is about 16cm tall. Buyers can choose from a plush with an embroidered face or a printed face. Please allow 1-2 weeks for shipping; domestic shipping time is usually under one week while International shipping times can vary. NOTE: This product was made in a household with dogs. Also, as these are handmade, they may not be identical to the one pictured.',
     hero: 'assets/images/wind-warrior/Wind-Warrior-1.jpg',
     images: [
